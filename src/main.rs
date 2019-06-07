f3589aa68001bbc52c1cde00bf300323b28ee6e7
leo60228 <iakornfeld@gmail.com>
2019-06-07 16:44:14 -0400

    Autogenerate full handle declaration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,90 @@
 use clang::*;
 use walkdir::WalkDir;
 use heck::SnakeCase;
+use std::collections::HashSet;
+
+fn invoke_handle<'tu>(functions: sonar::Functions<'tu>, prefix: &str) {
+    let mut init = None;
+    let mut exit = None;
+
+    for func in functions {
+        if !func.name.starts_with(prefix) { continue; }
+
+        if func.name.ends_with("Initialize") || func.name.ends_with("Init") {
+            init = Some(func);
+        } else if func.name.ends_with("Exit") {
+            exit = Some(func);
+        }
+
+        if init.is_some() && exit.is_some() { break; }
+    }
+
+    let init = init.unwrap();
+    let exit = exit.unwrap();
+
+    print!("handle!(");
+
+    if init.entity.get_result_type().unwrap().get_display_name() == "Result" {
+        print!("0");
+    } else {
+        print!("_");
+    }
+
+    print!(" in sys::{}(", init.name);
+
+    let mut first = true;
+
+    for argument in init.entity.get_arguments().unwrap() {
+        if !first {
+            print!(", ");
+        }
+
+        first = true;
+
+        let typ = argument.get_type().unwrap();
+        if typ.get_pointee_type().is_some() {
+            if typ.get_display_name().starts_with("const ") {
+                print!("ptr::null()");
+            } else {
+                print!("ptr::null_mut()");
+            }
+        } else {
+            print!("Default::default()");
+        }
+    }
+
+    print!("), sys::{}(", exit.name);
+
+    for argument in exit.entity.get_arguments().unwrap() {
+        if !first {
+            print!(", ");
+        }
+
+        first = true;
+
+        let typ = argument.get_type().unwrap();
+        if typ.get_pointee_type().is_some() {
+            if typ.get_display_name().starts_with("const ") {
+                print!("ptr::null()");
+            } else {
+                print!("ptr::null_mut()");
+            }
+        } else {
+            print!("Default::default()");
+        }
+    }
+
+    println!("), {{");
+}
 
 fn main() {
     let prefix = std::env::args().nth(1);
+
+    if prefix.is_some() {
+        println!("use crate::sys;");
+        println!("use crate::macros::handle;");
+        println!("use std::ptr;\n");
+    }
 
     let clang = Clang::new().unwrap();
     let index = Index::new(&clang, false, false);
@@ -19,6 +100,13 @@ fn main() {
 
     let entities: Vec<_> = units.iter().flat_map(|e| e.get_entity().get_children()).collect();
 
+    if let Some(prefix) = &prefix {
+        invoke_handle(sonar::find_functions(entities.clone()), prefix);
+    }
+
+    let mut exported_types = HashSet::new();
+    let mut first = true;
+
     let functions = sonar::find_functions(entities);
     for function in functions {
         let func_name = (if let Some(prefix) = &prefix {
@@ -27,9 +115,18 @@ fn main() {
             if function.name.ends_with("Exit") { continue; }
             (&function.name[prefix.len()..]).to_string()
         } else {
-            function.name
+            function.name.clone()
         }).to_snake_case();
 
+        if !first {
+            print!("\n");
+        }
+
+        first = false;
+
+        let mut args = Vec::new();
+
+        if prefix.is_some() { print!("    "); }
         print!("pub fn {}(&mut self", &func_name);
 
         for argument in function.entity.get_arguments().unwrap_or_else(Vec::new) {
@@ -41,6 +138,8 @@ fn main() {
                 (false, typ)
             };
 
+            args.push(argument.get_name().unwrap());
+
             print!(", {}: ", argument.get_name().unwrap());
 
             let typ_name = match typ.get_display_name().as_ref() {
@@ -48,7 +147,20 @@ fn main() {
                 "int" => "i32".to_string(),
                 "void" => "()".to_string(),
                 "const void" => "const ()".to_string(),
-                other => other.to_string(),
+                "u8" => "u8".to_string(),
+                "u16" => "u16".to_string(),
+                "u32" => "u32".to_string(),
+                "u64" => "u64".to_string(),
+                "i8" => "i8".to_string(),
+                "i16" => "i16".to_string(),
+                "i32" => "i32".to_string(),
+                "i64" => "i64".to_string(),
+                "const char" => "const char".to_string(),
+                "char" => "char".to_string(),
+                other => {
+                    exported_types.insert(other.to_string());
+                    other.to_string()
+                },
             };
 
             if is_pointer && typ_name.starts_with("const") {
@@ -85,6 +197,34 @@ fn main() {
             }
         }
 
-        println!("{{ unimplemented!() }}");
+        println!("{{");
+
+        if prefix.is_some() { print!("    "); }
+        print!("    sys::{}(", function.name);
+
+        let mut first = true;
+
+        for arg in args {
+            if !first {
+                print!(", ");
+            }
+
+            first = false;
+
+            print!("{}", arg);
+        }
+
+        println!(").into()");
+
+        if prefix.is_some() { print!("    "); }
+        println!("}}");
+    }
+
+    if prefix.is_some() {
+        println!("}});\n");
+
+        for export in exported_types {
+            println!("pub use sys::{};", export);
+        }
     }
 }
